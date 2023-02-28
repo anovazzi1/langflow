7b76b4eb853262df377f83973ab409d1b2eec841
anovazzi1 <otavio2204@gmail.com>
2023-02-28 17:56:05 -0300

    tabs manager refactor

--- space_flow/src/pages/FlowPage/flowManager/index.tsx
+++ space_flow/src/pages/FlowPage/flowManager/index.tsx
@@ -3,47 +3,48 @@ import { ReactFlowProvider } from "reactflow";
 import FlowPage from "..";
 import { TabsContext } from "../../../contexts/tabsContext";
 import TabComponent from "./tabComponent";
-import { example } from "../../../data_assets/example";
 var _ = require("lodash");
 
 export function TabsManager() {
-  const { flows, addFlow, tabIndex, setTabIndex } = useContext(TabsContext);
-  useEffect(() => {
-    if (flows.length === 0) {
-      addFlow();
-    }
-  }, [addFlow, flows.length]);
+	const { flows, addFlow, tabIndex, setTabIndex } = useContext(TabsContext);
+	useEffect(() => {
+		if (flows.length === 0) {
+			addFlow();
+		}
+	}, [addFlow, flows.length]);
 
-  return (
-    <div className="h-full w-full flex flex-col">
-      <div className="w-full flex pr-2 flex-row text-center items-center bg-gray-100 px-2">
-        {flows.map((flow, index) => {
-          return (
-            <TabComponent
-              onClick={() => setTabIndex(index)}
-              selected={index === tabIndex}
-              key={index}
-              flow={flow}
-            />
-          );
-        })}
-        <TabComponent
-          onClick={() => {
-            addFlow();
-          }}
-          selected={false}
-          flow={null}
-        />
-      </div>
-      <div className="w-full h-full">
-        <ReactFlowProvider>
-          {flows[tabIndex] ? (
-            <FlowPage flow={flows[tabIndex]}></FlowPage>
-          ) : (
-            <></>
-          )}
-        </ReactFlowProvider>
-      </div>
-    </div>
-  );
+	return (
+		<div className="h-full w-full flex flex-col">
+			<div className="w-full flex pr-2 flex-row text-center items-center bg-gray-100 px-2">
+				{flows.map((flow, index) => {
+					//tabs for flows
+					return (
+						<TabComponent
+							onClick={() => setTabIndex(index)}
+							selected={index === tabIndex}
+							key={index}
+							flow={flow}
+						/>
+					);
+				})}
+				{/* add new flow button */}
+				<TabComponent
+					onClick={() => {
+						addFlow();
+					}}
+					selected={false}
+					flow={null}
+				/>
+			</div>
+			<div className="w-full h-full">
+				<ReactFlowProvider>
+					{flows[tabIndex] ? (
+						<FlowPage flow={flows[tabIndex]}></FlowPage>
+					) : (
+						<></>
+					)}
+				</ReactFlowProvider>
+			</div>
+		</div>
+	);
 }
